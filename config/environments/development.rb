fdb7e16be646adef6622b99659c89f17c069bd6a
Marcos <marcintosh87@gmail.com>
2022-02-10 14:04:39 -0500

    adjusted environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -73,4 +74,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # config.hosts << "9f17-66-44-50-42.ngrok.io"
+  
 end
